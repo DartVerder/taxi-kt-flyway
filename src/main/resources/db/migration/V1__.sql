30557a8b64a6c69bae39d75fac838d1197680857
Vera Beketova <v.beketova@haulmont.com>
2022-02-08 18:18:13 +0400

    creating migration files

---
+++ src/main/resources/db/migration/V1__.sql
@@ -0,0 +1,181 @@
+CREATE SEQUENCE IF NOT EXISTS hibernate_sequence START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE address
+(
+    id           BIGINT       NOT NULL,
+    house_number VARCHAR(255) NOT NULL,
+    street       VARCHAR(255) NOT NULL,
+    city         VARCHAR(255) NOT NULL,
+    entrance     INTEGER,
+    CONSTRAINT pk_address PRIMARY KEY (id)
+);
+
+CREATE TABLE car
+(
+    id                BIGINT       NOT NULL,
+    license_plate     VARCHAR(12)  NOT NULL UNIQUE,
+    driver_id         BIGINT       NOT NULL,
+    colour            VARCHAR(255) NOT NULL,
+    car_model_id      BIGINT       NOT NULL,
+    production_date   date,
+    comfort_class     INTEGER      NOT NULL DEFAULT 0,
+    child_safety_seat INTEGER DEFAULT 0,
+    CONSTRAINT pk_car PRIMARY KEY (id)
+);
+
+CREATE TABLE car_model
+(
+    id    BIGINT       NOT NULL,
+    model VARCHAR(255) NOT NULL,
+    brand VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_car_model PRIMARY KEY (id)
+);
+
+CREATE TABLE card
+(
+    id          BIGINT       NOT NULL,
+    card_number VARCHAR(255) NOT NULL,
+    expire_date date         NOT NULL,
+    cvc_cvv     INTEGER      NOT NULL,
+    CONSTRAINT pk_card PRIMARY KEY (id)
+);
+
+CREATE TABLE driver
+(
+    id                  BIGINT           NOT NULL,
+    first_name          VARCHAR(255)     NOT NULL,
+    last_name           VARCHAR(255)     NOT NULL,
+    date_of_birth       date             NOT NULL,
+    rating              DOUBLE PRECISION NOT NULL DEFAULT 5.0,
+    card_id             BIGINT           NOT NULL,
+    drivers_license     VARCHAR(255) NOT NULL UNIQUE,
+    certification_level INTEGER,
+    position_id         BIGINT,
+    driver_id           BIGINT,
+    CONSTRAINT pk_driver PRIMARY KEY (id)
+);
+
+CREATE TABLE fare
+(
+    id                 BIGINT           NOT NULL,
+    name               VARCHAR(255)     NOT NULL,
+    city               VARCHAR(255)     NOT NULL,
+    price_for_distance DOUBLE PRECISION NOT NULL,
+    price_for_waiting  DOUBLE PRECISION NOT NULL,
+    CONSTRAINT pk_fare PRIMARY KEY (id)
+);
+
+CREATE TABLE passenger
+(
+    id            BIGINT           NOT NULL,
+    first_name    VARCHAR(255)     NOT NULL,
+    last_name     VARCHAR(255)     NOT NULL,
+    date_of_birth date             NOT NULL,
+    rating        DOUBLE PRECISION NOT NULL DEFAULT 5.0,
+    card_id       BIGINT           NOT NULL,
+    discount      DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    CONSTRAINT pk_passenger PRIMARY KEY (id)
+);
+
+CREATE TABLE payment
+(
+    id           BIGINT  NOT NULL,
+    payment_type INTEGER NOT NULL,
+    success      BOOLEAN NOT NULL DEFAULT false,
+    payment_date date    NOT NULL,
+    CONSTRAINT pk_payment PRIMARY KEY (id)
+);
+
+CREATE TABLE position
+(
+    id        BIGINT           NOT NULL,
+    latitude  DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    CONSTRAINT pk_position PRIMARY KEY (id)
+);
+
+CREATE TABLE price
+(
+    id           BIGINT           NOT NULL,
+    fare_id      BIGINT           NOT NULL,
+    waiting_time INTEGER DEFAULT 0,
+    full_price   DOUBLE PRECISION NOT NULL,
+    CONSTRAINT pk_price PRIMARY KEY (id)
+);
+
+CREATE TABLE trip
+(
+    id                  BIGINT                      NOT NULL,
+    trip_request_id     BIGINT                      NOT NULL,
+    driver_id           BIGINT                      NOT NULL,
+    payment_id          BIGINT                      NOT NULL,
+    arriving_datetime   TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    start_trip_datetime TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    end_trip_datetime   TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    CONSTRAINT pk_trip PRIMARY KEY (id)
+);
+
+CREATE TABLE trip_addresses
+(
+    id            BIGINT  NOT NULL,
+    order_in_trip INTEGER NOT NULL,
+    address_id    BIGINT,
+    trip_id       BIGINT,
+    CONSTRAINT pk_trip_addresses PRIMARY KEY (id)
+);
+
+CREATE TABLE trip_request
+(
+    id                   BIGINT                      NOT NULL,
+    datetime_of_creation TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    price_id             BIGINT                      NOT NULL,
+    passenger_id         BIGINT,
+    distance DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    is_cancelled        BOOLEAN DEFAULT false,
+    CONSTRAINT pk_trip_request PRIMARY KEY (id)
+);
+
+ALTER TABLE card
+    ADD CONSTRAINT uc_card_card_number UNIQUE (card_number);
+
+ALTER TABLE car
+    ADD CONSTRAINT FK_CAR_ON_CAR_MODEL FOREIGN KEY (car_model_id) REFERENCES car_model (id);
+
+ALTER TABLE car
+    ADD CONSTRAINT FK_CAR_ON_DRIVER FOREIGN KEY (driver_id) REFERENCES driver (id);
+
+ALTER TABLE driver
+    ADD CONSTRAINT FK_DRIVER_ON_CARD FOREIGN KEY (card_id) REFERENCES card (id);
+
+ALTER TABLE driver
+    ADD CONSTRAINT FK_DRIVER_ON_DRIVER FOREIGN KEY (driver_id) REFERENCES driver (id);
+
+ALTER TABLE driver
+    ADD CONSTRAINT FK_DRIVER_ON_POSITION FOREIGN KEY (position_id) REFERENCES position (id);
+
+ALTER TABLE passenger
+    ADD CONSTRAINT FK_PASSENGER_ON_CARD FOREIGN KEY (card_id) REFERENCES card (id);
+
+ALTER TABLE price
+    ADD CONSTRAINT FK_PRICE_ON_FARE FOREIGN KEY (fare_id) REFERENCES fare (id);
+
+ALTER TABLE trip_addresses
+    ADD CONSTRAINT FK_TRIP_ADDRESSES_ON_ADDRESS FOREIGN KEY (address_id) REFERENCES address (id);
+
+ALTER TABLE trip_addresses
+    ADD CONSTRAINT FK_TRIP_ADDRESSES_ON_TRIP FOREIGN KEY (trip_id) REFERENCES trip (id);
+
+ALTER TABLE trip
+    ADD CONSTRAINT FK_TRIP_ON_DRIVER FOREIGN KEY (driver_id) REFERENCES driver (id);
+
+ALTER TABLE trip
+    ADD CONSTRAINT FK_TRIP_ON_PAYMENT FOREIGN KEY (payment_id) REFERENCES payment (id);
+
+ALTER TABLE trip
+    ADD CONSTRAINT FK_TRIP_ON_TRIP_REQUEST FOREIGN KEY (trip_request_id) REFERENCES trip_request (id);
+
+ALTER TABLE trip_request
+    ADD CONSTRAINT FK_TRIP_REQUEST_ON_PASSENGER FOREIGN KEY (passenger_id) REFERENCES passenger (id);
+
+ALTER TABLE trip_request
+    ADD CONSTRAINT FK_TRIP_REQUEST_ON_PRICE FOREIGN KEY (price_id) REFERENCES price (id);
---
+++ src/main/resources/db/migration/V2__.sql
@@ -0,0 +1,92 @@
+INSERT INTO card (id, card_number, expire_date, cvc_cvv)
+VALUES (1, '374245455400126', '2024-01-01', 222);
+
+INSERT INTO card (id, card_number, expire_date, cvc_cvv)
+VALUES (2, '374245455400127', '2027-01-01', 432);
+
+INSERT INTO card (id, card_number, expire_date, cvc_cvv)
+VALUES (3, '374245455400128', '2025-04-01', 421);
+
+INSERT INTO card (id, card_number, expire_date, cvc_cvv)
+VALUES (4, '374245455400129', '2025-03-01', 454);
+
+INSERT INTO driver (id, first_name, last_name, date_of_birth, rating, drivers_license, card_id)
+VALUES (1, 'Ivan', 'Ivanov', '1970-02-01', 4.9, '123123', 1);
+
+INSERT INTO driver (id, first_name, last_name, date_of_birth, drivers_license, card_id)
+VALUES (2, 'Petr', 'Petrov', '1995-06-16', '123124', 2);
+
+INSERT INTO passenger (id, first_name, last_name, date_of_birth, rating, card_id)
+VALUES (1, 'Maria', 'Ivanova', '1980-01-01', 5.0, 3);
+
+INSERT INTO passenger (id, first_name, last_name, date_of_birth, rating, card_id)
+VALUES (2, 'Anton', 'Kabanov', '1996-03-30', 4.5, 4);
+
+INSERT INTO car_model (id, model, brand)
+VALUES (1, 'Vesta', 'LADA');
+
+INSERT INTO car_model (id, model, brand)
+VALUES (2, 'Volkswagen', 'Polo');
+
+INSERT INTO car (id, colour, production_date, comfort_class, license_plate, driver_id, car_model_id)
+VALUES (1, 'red', NOW(), 0, 'A123BC163', 1, 1);
+
+INSERT INTO car (id, colour, production_date, comfort_class, license_plate, driver_id, car_model_id)
+VALUES (2, 'black', '2021-01-01', 2, 'A444BC163', 2, 2);
+
+INSERT INTO address (id, house_number, entrance, city, street)
+VALUES (1, '2', 1, 'Samara', 'Kuybisheva');
+
+INSERT INTO address (id, house_number, entrance, city, street)
+VALUES (2, '32', 2, 'Samara', 'Venceka');
+
+INSERT INTO address (id, house_number, city, street)
+VALUES (3, '1', 'Samara', 'Tashkentskaya');
+
+INSERT INTO address (id, house_number, city, street)
+VALUES (4, '55', 'Samara', 'Kuybisheva');
+
+INSERT INTO fare (id, name, city, price_for_distance, price_for_waiting)
+VALUES (1, 'economy', 'Samara', 12, 5);
+
+INSERT INTO fare (id, name, city, price_for_distance, price_for_waiting)
+VALUES (2, 'comfort', 'Samara', 20, 5);
+
+INSERT INTO price (id, full_price, waiting_time, fare_id)
+VALUES (1, 200, 0, 1);
+
+INSERT INTO price (id, full_price, waiting_time, fare_id)
+VALUES (2, 400, 3, 2);
+
+INSERT INTO trip_request (id, datetime_of_creation, price_id, passenger_id)
+VALUES (1, NOW(), 1, 1);
+
+INSERT INTO trip_request (id, datetime_of_creation, price_id, passenger_id)
+VALUES (2, NOW(), 2, 2);
+
+INSERT INTO payment (id, payment_type, success, payment_date)
+VALUES (1, 1, FALSE, NOW());
+
+INSERT INTO payment (id, payment_type, success, payment_date)
+VALUES (2, 1, TRUE, NOW());
+
+INSERT INTO trip (id, trip_request_id, payment_id, arriving_datetime, start_trip_datetime, end_trip_datetime, driver_id)
+VALUES (1, 1, 1, NOW(), NOW(), NOW(), 1);
+
+INSERT INTO trip (id, trip_request_id, payment_id, arriving_datetime, start_trip_datetime, end_trip_datetime, driver_id)
+VALUES (2, 2, 2, NOW(), NOW(), NOW(), 2);
+
+INSERT INTO trip_addresses (id, order_in_trip, address_id, trip_id)
+VALUES (1, 0, 1, 1);
+
+INSERT INTO trip_addresses (id, order_in_trip, address_id, trip_id)
+VALUES (2, 1, 2, 1);
+
+INSERT INTO trip_addresses (id, order_in_trip, address_id, trip_id)
+VALUES (3, 1, 3, 2);
+
+INSERT INTO trip_addresses (id, order_in_trip, address_id, trip_id)
+VALUES (4, 2, 4, 2);
+
+INSERT INTO trip_addresses (id, order_in_trip, address_id, trip_id)
+VALUES (5, 3, 1, 2);
